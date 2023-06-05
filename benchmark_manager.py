b046c6974e7ea9d234079adca9c8f2a6dd0a1ac8
steve <>
2023-06-05 01:46:07 +0000

    cleaning up code (using ChatGPT)

--- benchmark_manager.py
+++ benchmark_manager.py
@@ -5,30 +5,76 @@ import threading
 from benchmark_utils import generate_one_completion, run_benchmark
 
 def print_server_output(process):
+    """
+    This function prints the output of a subprocess in real-time.
+    
+    :param process: The subprocess whose output should be printed.
+    """
     for line in process.stdout:
         print(line, end='')
 
-def run_benchmark_workflow(model_name, portnum, group_size=None, maxnum=-1, 
-                           prompt_type="long", user_tag="### Instruction:", 
-                           assistant_tag="### Response:", system_prefix="", experiment_tag=""):
-    # Create the base command list
-    command = ['python', 'server.py', '--api', '--model', model_name, '--api-blocking-port', str(portnum), '--api-streaming-port', str(portnum+1),  '--listen-port', str(portnum+2), '--wbits', str(4)]
 
-    # If group_size is not None, append it to the command list
+def start_server(model_name, portnum, group_size=None, working_directory='text-generation-webui'):
+    """
+    This function starts a server in a new process.
+    
+    :param model_name: The name of the model to be used by the server.
+    :param portnum: The base port number to be used by the server.
+    :param group_size: The group size to be used by the server.
+    :param working_directory: The working directory in which the server script resides.
+    :return: The subprocess in which the server is running.
+    """
+    # Create the base command list
+    command = [
+        'python', 'server.py', 
+        '--api', 
+        '--model', model_name, 
+        '--api-blocking-port', str(portnum), 
+        '--api-streaming-port', str(portnum+1),  
+        '--listen-port', str(portnum+2), 
+        '--wbits', str(4)
+    ]
+
+    # If group_size is provided, append it to the command list
     if group_size is not None:
         command += ['--groupsize', str(group_size)]
 
     print("Starting server...")
-    # Start server.py in a separate process
-    server_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, cwd='text-generation-webui')
+    
+    # Start the server in a new process
+    server_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, cwd=working_directory)
 
-    # Wait for "Starting API" to be printed before running the benchmark
+    # Wait for "Starting API" to be printed before proceeding
     for line in iter(server_process.stdout.readline, ''):
         print(line, end='')  # Optionally print the server output
         if "Starting API" in line:
             break
 
     print("Server started!")
+    
+    return server_process
+
+
+def run_benchmark_workflow(model_name, portnum, group_size=None, maxnum=-1, 
+                           prompt_type="long", user_tag="### Instruction:", 
+                           assistant_tag="### Response:", system_prefix="", experiment_tag="", 
+                           working_directory='text-generation-webui'):
+    """
+    This function manages the complete benchmark workflow, including starting the server, running the benchmark, and stopping the server.
+    
+    :param model_name: The name of the model to be used.
+    :param portnum: The base port number to be used by the server.
+    :param group_size: The group size to be used by the server.
+    :param maxnum: The maximum number of items to process during the benchmark.
+    :param prompt_type: The type of prompt to use during the benchmark.
+    :param user_tag: The user tag to use during the benchmark.
+    :param assistant_tag: The assistant tag to use during the benchmark.
+    :param system_prefix: The system prefix to use during the benchmark.
+    :param experiment_tag: The experiment tag to use during the benchmark.
+    :param working_directory: The working directory in which the server script resides.
+    """
+    # Start the server
+    server_process = start_server(model_name, portnum, group_size=group_size, working_directory=working_directory)
 
     # Create a separate thread to print the server output
     print_thread = threading.Thread(target=print_server_output, args=(server_process,))
@@ -37,7 +83,8 @@ def run_benchmark_workflow(model_name, portnum, group_size=None, maxnum=-1,
     # Run the benchmark
     run_benchmark(model_name+"_"+experiment_tag, maxnum, portnum, prompt_type, user_tag, assistant_tag, system_prefix)
 
-    # Once the benchmark has finished running, kill the server process
+    # Once the benchmark has finished running, terminate the server process
     os.kill(server_process.pid, signal.SIGTERM)
+    
     # Wait for the print_thread to finish
     print_thread.join()
